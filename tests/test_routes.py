0122a1b741b6d4980e8d448136726b6c9c252024
zakbaum <zachbaum@ibm.com>
2024-11-24 08:52:49 -0500

    implement account update story.

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -147,4 +147,23 @@ class TestAccountService(TestCase):
         data = resp.get_json()
         self.assertEqual(len(data), 5)
 
-    
+    def test_update_account(self):
+        """It should Update an existing Account"""
+        # create an Account to update
+        test_account = AccountFactory()
+        resp = self.client.post(BASE_URL, json=test_account.serialize())
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+
+        # update the account
+        new_account = resp.get_json()
+        new_account["name"] = "Something Known"
+        resp = self.client.put(f"{BASE_URL}/{new_account['id']}", json=new_account)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        updated_account = resp.get_json()
+        self.assertEqual(updated_account["name"], "Something Known")
+
+    def test_update_account_not_found(self):
+        """It should not Update an Account that is not found"""
+        # Assert account not found 
+        resp = self.client.put(f"{BASE_URL}/0", json={"name": "Something Not Known"})
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
